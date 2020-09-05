d9a4e598e63af39f7099baaf444c1705c520f039
takayukioda <takayuki@odashi.me>
2020-09-05 11:08:29 +0900

    feat: add swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('Athletics OpenAPI')
+    .setDescription('Athletics API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/openapi', app, document, {
+    customSiteTitle: 'Swagger UI for athletics',
+    swaggerOptions: { docExpansion: 'none' },
+  });
+
+  await app.listen(9000);
 }
 bootstrap();
