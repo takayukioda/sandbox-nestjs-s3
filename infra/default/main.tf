ec523bd89c2217762d3e77b255d3e0adc1e58c73
takayukioda <takayuki@odashi.me>
2020-09-06 10:58:24 +0900

    infra: configure s3 to upload

---
+++ infra/default/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_version = "~> 0.13.2"
+}
+
+variable aws_profile {
+  description = "The name of AWS profile to use"
+  default     = "default"
+}
+
+locals {
+  region    = "ap-northeast-1"
+  namespace = "sandbox-nestjs-s3"
+  env       = "default"
+
+  tags = {
+    Name        = "${local.namespace}-${local.env}"
+    Namespace   = local.namespace
+    Environment = local.env
+    Terraform   = true
+  }
+}
+
+provider aws {
+  version = "~> 3.0"
+  profile = var.aws_profile
+  region  = local.region
+}
+
+resource aws_s3_bucket storage {
+  bucket = "sandbox-nestjs-s3-storage"
+  acl    = "private"
+
+  tags = merge(local.tags, {
+    Name = "${local.namespace}-s3-${local.env}"
+  })
+}
+
+resource aws_s3_bucket_public_access_block default {
+  bucket = aws_s3_bucket.storage.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
